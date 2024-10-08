8ced6c125bfb1b28952ff9d1ba7b84cfae9ceb11
Aathirainathan <113375504+Aathirainathan@users.noreply.github.com>
2024-10-08 16:24:07 +0530

    Update appointment.py

--- Hospital Management System/entity/appointment.py
+++ Hospital Management System/entity/appointment.py
@@ -20,3 +20,19 @@ class Appointment:
 
     def get_description(self):
         return self.__description
+    
+    def set_appointment_id(self,appointment_id):
+        self.__appointment_id=appointment_id
+        
+    def set_patient_id(self,patient_id):
+        self.__patient_id=patient_id
+        
+    def set_doctor_id(self,doctor_id):
+        self.__doctor_id=doctor_id
+    
+    def set_appointment_date(self,appointment_date):
+        self.__appointment_date=appointment_date
+        
+    def set_description(self,description):
+        self.__description=description
+    
